3a6ff422856f7099a2ae29a592ee4ba3e156576e
MalcolmSmalls <smallsbeats@gmail.com>
2022-12-04 13:07:06 -0800

    Add new App and test to check if button changes title on click to matching text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Our First Test</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React from "react"
+
+export default function App () {
+  const [ title, setTitle ] = React.useState('Magnificent Monkeys')
+
+  function handleClick () {
+    setTitle('Radical Rhinos')
+  }
+
   return (
-    <div className="App">
-      <h1>Our First Test</h1>
-    </div>
-  );
-}
 
-export default App;
+    <div>
+      <h1>{title}</h1>
+      <button onClick={handleClick}>Change title</button>
+    </div>
+
+  )
+}
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 describe("App component", () => {
-    it("renders correct heading", () => {
-        const { getByRole } = render(<App />);
-        expect(getByRole("heading").textContent).toMatch(/our first test/i);
+    it("renders magnificent monkeys", () => {
+        const { container } = render(<App />);
+        expect(container).toMatchSnapshot()
     });
+
+    it("renders radical rhinos after click", () => {
+        render(<App />);
+        const button = screen.getByRole("button", { name: "Change title"});
+
+        userEvent.click(button);
+
+        expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i)
+
+
+    })
 });
